3165aee4090f54794a9d3b284faaf3c667d07fc1
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-04-26 01:07:46 +0300

    I fixed the bugs again, but it seems to be successful, the server starts

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -1,5 +1,31 @@
 package ru.risdeveau.geotracker
 
-fun main(){
-    println("Hello")
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    val host = "0.0.0.0"
+
+    embeddedServer(
+        factory = Netty,
+        port = port,
+        host = host,
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello from GeoTracker API!")
+        }
+
+        get("/api/status") {
+            call.respond(mapOf("status" to "OK", "version" to "0.0.1"))
+        }
+    }
 }
