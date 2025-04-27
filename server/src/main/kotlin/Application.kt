01b060e5d1a5f13956a0b92cfdfb20efedaf39d9
Kirill <kremizov3@gmail.com>
2025-04-27 14:10:33 +0300

    fix(ru.risdeveau.geotracker): change includes
    Not used includes has been deleted

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -1,10 +1,6 @@
 package ru.risdeveau.geotracker
-
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
 import ru.risdeveau.geotracker.Routers.LoginRoutings;
 import ru.risdeveau.geotracker.Routers.module;
--- server/src/main/kotlin/Routers/moduleRouts.kt
+++ server/src/main/kotlin/Routers/moduleRouts.kt
@@ -1,7 +1,6 @@
 package ru.risdeveau.geotracker.Routers
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 import ru.risdeveau.geotracker.Controllers.moduleController;
