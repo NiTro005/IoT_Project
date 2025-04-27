f6ad0ce8fa681373898f10f7e675ea12b937ec38
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-04-27 14:45:43 +0300

    Slightly fixed the Routs module

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -1,18 +1,9 @@
 package ru.risdeveau.geotracker
 
-import io.github.jan.supabase.postgrest.postgrest
-import io.github.jan.supabase.postgrest.query.Count
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import ru.risdeveau.geotracker.data_base.LocationData
-import ru.risdeveau.geotracker.data_base.Supabase
+import ru.risdeveau.geotracker.Routers.module
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
@@ -26,32 +17,3 @@ fun main() {
     ).start(wait = true)
 }
 
-fun Application.module() {
-    install(ContentNegotiation) {
-        json()
-    }
-    routing {
-        get("/") {
-            call.respondText("Server is running")
-        }
-
-        get("/check-db-connection") {
-            try {
-                val result = Supabase.client.postgrest["locations"]
-                    .select(count = Count.EXACT)
-                    .decodeSingle<Map<String, Int>>()
-
-                call.respond(mapOf(
-                    "status" to "OK",
-                    "message" to "Database connection successful",
-                    "count" to (result["count"] ?: 0)
-                ))
-            } catch (e: Exception) {
-                call.respond(mapOf(
-                    "status" to "error",
-                    "message" to "Database connection failed: ${e.message}"
-                ))
-            }
-        }
-    }
-}
--- server/src/main/kotlin/Routers/moduleRouts.kt
+++ server/src/main/kotlin/Routers/moduleRouts.kt
@@ -1,13 +1,17 @@
 package ru.risdeveau.geotracker.Routers
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 
 import ru.risdeveau.geotracker.Controllers.moduleController;
 
 fun Application.module() {
     val moduleController = moduleController();
-
+    install(ContentNegotiation) {
+        json()
+    }
     routing {
         get("/",){
             moduleController.working(call);
