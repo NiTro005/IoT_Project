31be66fe38d1f4ec972aaa6202e643b47ebb96b8
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-05-01 21:32:50 +0300

    Configured the environment variable to work correctly

--- server/src/main/kotlin/config/Supabase.kt
+++ server/src/main/kotlin/config/Supabase.kt
@@ -3,11 +3,14 @@ package ru.risdeveau.geotracker.config
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.cdimascio.dotenv.dotenv
 
 
 object Supabase {
     private const val SUPABASE_URL = "https://rumvwrachgjguyqejuhs.supabase.co"
-    private val SUPABASE_KEY = System.getenv("SUPABASE_KEY")?: throw Exception("Data base key not found")
+    private val SUPABASE_KEY = dotenv()["SUPABASE_KEY"]
+        ?:System.getenv("SUPABASE_KEY")
+        ?: throw Exception("Data base key not found")
 
     init{
         print(SUPABASE_KEY)
