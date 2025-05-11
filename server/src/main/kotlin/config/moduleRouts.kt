91f1629a9598617aa18fe27cbce2cd33371bf462
Kirill <kremizov3@gmail.com>
2025-05-11 12:25:12 +0300

    feat(server/config/moduleRouts): plug in new logic module

--- server/src/main/kotlin/config/moduleRouts.kt
+++ server/src/main/kotlin/config/moduleRouts.kt
@@ -4,10 +4,12 @@ import io.ktor.server.application.*
 import ru.risdeveau.geotracker.routers.DBRoutes
 import ru.risdeveau.geotracker.routers.mapRouts
 import ru.risdeveau.geotracker.routers.testRouts
+import ru.risdeveau.geotracker.routers.appRouts
 
 fun Application.module() {
     configureSurealization()
     DBRoutes()
     mapRouts()
     testRouts()
+    appRouts()
 }
