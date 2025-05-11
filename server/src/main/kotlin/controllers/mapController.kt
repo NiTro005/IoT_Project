e3a26ee91dddae9650121cd2f4d91b01a00ae5fd
Kirill <kremizov3@gmail.com>
2025-05-11 12:08:20 +0300

    fix(server/controllers/mapController):rewrite logic for post request of all users
    Now it works with DB

--- server/src/main/kotlin/controllers/mapController.kt
+++ server/src/main/kotlin/controllers/mapController.kt
@@ -4,11 +4,17 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import ru.risdeveau.geotracker.models.dto.LocationData
+import java.io.File
+import java.time.LocalDate
+
+import ru.risdeveau.geotracker.models.repositories.LocationRepos.getAllUsersInOneDay
+import ru.risdeveau.geotracker.models.repositories.LocationRepos.getUserInOneDay
 
 class mapController {
     suspend fun mapPost(call: ApplicationCall){
-        val frontData = LocationData("kikita", 23.3, 11.4);//TODO("Получние данных по нику пользователя из БД")
-        call.respond(HttpStatusCode.OK, frontData)
+        val today: String = LocalDate.now().toString();
+        val data : List<LocationData> = getAllUsersInOneDay(today)
+        call.respond(HttpStatusCode.OK, data)
     }
 
     suspend fun mapGet(call: ApplicationCall){
