f2a16da0e054a20a22b1191dd22b86aa3b6e4495
Kirill <kremizov3@gmail.com>
2025-05-14 23:00:33 +0300

    fix(controllers/mapcontroller): deleted useless methods

--- server/src/main/kotlin/controllers/mapController.kt
+++ server/src/main/kotlin/controllers/mapController.kt
@@ -1,33 +1,15 @@
 package ru.risdeveau.geotracker.controllers
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import ru.risdeveau.geotracker.models.dto.LocationData
 import java.io.File
-import java.time.LocalDate
 
-import ru.risdeveau.geotracker.models.repositories.LocationRepos.getAllUsersInOneDay
-import ru.risdeveau.geotracker.models.repositories.LocationRepos.getUserInOneDay
 
 class mapController {
-    suspend fun mapPost(call: ApplicationCall){
-        val today: String = LocalDate.now().toString();
-        val data : List<LocationData> = getAllUsersInOneDay(today)
-        call.respond(HttpStatusCode.OK, data)
-    }
-
-    suspend fun mapUserPost (call: ApplicationCall) {
-        val user_name = call.parameters["user_name"].toString()
-        val today: String = LocalDate.now().toString();
-        val data : List<LocationData> = getUserInOneDay(today, user_name)
-        call.respond(HttpStatusCode.OK, data)
-    }
 
     suspend fun mapGet(call: ApplicationCall){
         val filePath = "static/index.html"
         val file = File(this::class.java.classLoader.getResource(filePath)!!.toURI())
         call.respondFile(file)
     }
-    
 }
