84d74f7076ca686927ba5467d81e854eab5d6156
Kirill <kremizov3@gmail.com>
2025-05-14 22:58:07 +0300

    feat(Routs/mapRouts): Add web-soket

--- server/src/main/kotlin/routers/mapRouts.kt
+++ server/src/main/kotlin/routers/mapRouts.kt
@@ -3,22 +3,17 @@ package ru.risdeveau.geotracker.routers
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import ru.risdeveau.geotracker.controllers.mapController
+import ru.risdeveau.geotracker.controllers.WebSocketController
 
 fun Application.mapRouts() {
     val mapController = mapController();
+    val webSocketController = WebSocketController()
 
     routing {
-
+        webSocketController.setupWebSocketRoutes(this)
         get("/map") {
             mapController.mapGet(call);
         }
 
-        post("api/map"){
-            mapController.mapPost(call);
-        }
-
-        post("api/map/user/"){
-            mapController.mapUserPost(call);
-        }
     }
 }
